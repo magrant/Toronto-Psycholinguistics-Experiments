51b71b2412842d76676df05181b55698aa14a0df
neda <neda.alavi@mail.utoronto.ca>
2015-06-18 00:49:58 -0400

    Finished Text

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -136,68 +136,6 @@ var items = [
                             as: ["Knowledge", "Skill", "Math", "Research"]}],
 
      ["story", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" /><br><p>WAIT</p>', transfer: 6000}],
-     ["story", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" /> <br><p>CLICK</p>', transfer: "click"}],
+     ["story", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" /> <br><p>CLICK</p>', transfer: "click"}]
 
-
-    //
-    // Two "real" (i.e. non-filler) self-paced reading items with corresponding acceptability judgment items.
-    // There are two conditions.
-    //
-
-    [["s1",1], "DashedSentence", {s: "The journalist interviewed an actress who he knew to be shy of publicity after meeting on a previous occasion."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-    [["s2",1], "DashedSentence", {s: "The journalist interviewed an actress who after meeting on a previous occasion he knew to be shy of publicity."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-
-    // The first question will be chosen if the first sentence from the previous two items is chosen;
-    // the second question will be chosen if the second sentence from the previous pair of items is chosen.
-    [["q1",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?"}],
-    [["q2",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview her husband after meeting on a previous occasion?"}],
-
-    [["s1",2], "DashedSentence", {s: "The teacher helped struggling students who he encouraged to succeed without treating like idiots."},
-               "Question",       {q: "What did the teacher do?",
-                                  as: ["Encourage struggling students to succeed",
-                                       "Encourage his best students to succeed",
-                                       "Treat struffling students like idiots"]}],
-    [["s2",2], "DashedSentence", {s: "The teacher helped struggling students who without treating like idiots he encouraged to succeed."},
-               "Question",       {q: "What did the teacher do?", as: ["Encourage struggling students to succeed",
-                                                                      "Encourage his best students to succeed",
-                                                                      "Treat struggling students like idiots"]}],
-
-    [["q1",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage to succeed without treating their friends like idiots?"}}],
-    [["q2",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage their friends to succeed without treating like idiots?"}}],
-
-    //
-    // 10 self-paced-reading filler sentences.
-    //
-
-    ["f", "DashedSentence", {s: "The foreign spy that encoded the top-secret messages was given a new mission that required going to Japan."},
-          "Question",       {q: "The spy's mission required him to:", as: ["Go to Japan", "Destroy top-secret messages", "Bug a hotel room"]}],
-
-    ["f", "DashedSentence", {s: "The receptionist that the real estate company just hired immediately familiarized herself with all the phone numbers of their clients."},
-          "Question",       {q: "The receptionist familiarized herself with:",
-                             as: ["Some phone numbers",
-                                  "The health and safety regulations",
-                                  "Her boss"]}],
-
-    ["f", "DashedSentence", {s: "Only two specialized surgeons that work in the hospital could do this operation."},
-          "Question",       {q: "The operation can be performed by:",
-                             as: ["Two surgeons with specialist training",
-                                  "All the surgeons at the hospital",
-                                  "Three surgeons who are currently off sick"]}],
-
-    ["f", "DashedSentence", {s: "The gangsters that the local police officers tracked for years were represented by an inexperienced lawyer."},
-          "Question",       {q: "Who did the inexperienced lawyer represent?",
-                             as: ["Some gangsters", "Some local police officers", "A murder suspect"]}],
-
-    ["f", "DashedSentence", {s: "The woman that John had seen in the subway bought herself a pair of stunning shoes that cost a fortune."},
-          "Question",       {q: "Where did John see the woman?", as: ["In the subway", "On the bus", "In the shoe shop"]}],
-
-    ["f", "DashedSentence", {s: "The cowboy that the bulls tried to trample injured himself getting off a horse."}],
-
-    ["f", "DashedSentence", {s: "The patient that was admitted to the hospital last month still suffers severe pain in his left leg."},
-          "Question",       {q: "Which of the following is true?",
-                             as: ["The patient still has severe pain in his left leg",
-                                  "The patient still has severe pain in his right leg",
-                                  "The patient no longer suffers from pain in his left leg"]}]
 ];
