053dc60f012b92ee0f2fed44ec90f5417a1ad0a8
neda <neda.alavi@mail.utoronto.ca>
2015-06-21 17:17:39 -0400

    Added Image and Text items

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,4 +1,10 @@
-var shuffleSequence = seq(/*"intro",*/ sepWith("sep", seq("story", rshuffle("s1", "s2"))), sepWith("sep", rshuffle("q1", "q2")));
+/*
+--Toronto-Psycholinguistics-Experiments--
+
+Template that gives examples of everything Ibex can do for experiments
+*/
+
+var shuffleSequence = seq(/*"intro",*/ "practice", sepWith("sep", seq("story")), sepWith("sep", seq("image")));
 var practiceItemTypes = ["practice"];
 
 
@@ -12,7 +18,8 @@ var defaults = [
 
     "Message", {
         //"html" option is obligatory
-        hideProgressBar: false
+        hideProgressBar: false,
+        transfer: "keypress"
     },
 
     "DashedSentence", {
@@ -21,14 +28,11 @@ var defaults = [
           //other option: "speeded acceptability"
     },
 
-    "FlashSentence", {
-      //"s" option is obligatory
-      transfer: "keypress"
-    },
-
     "Question", {
         //"as" option is obligatory
         hasCorrect: true
+          //if a question has a correct answer,
+            //keep it as the first element of the "as" option
     },
 
     "AcceptabilityJudgment", {
@@ -96,95 +100,57 @@ var items = [
         }
     } ],
 
-    //
-    // Three practice items for self-paced reading (one with a comprehension question).
-    //
-    ["practice", "DashedSentence", {s: "This is a practice sentence to get you used to reading sentences like this."}],
-    ["practice", "DashedSentence", {s: "This is another practice sentence with a practice question following it."},
+
+    /*
+    ===================
+    TEXT
+    Controllers that work with Text and Questions
+    ===================
+    */
+
+    //text displayed word by word
+    ["practice", "DashedSentence", {s: "This is a practice sentence before the experiment begins."}],
+
+    //all text with MC question
+    ["story", "Message", {html: "<center>This is a story you can see all at once!<br><br>Tanya and John were racing. She won.<br><b>Press any key to continue.</b></center>"},
                  "Question", {hasCorrect: false, randomOrder: false,
+                              //if a question has a correct answer (hasCorrect: true), you would have to put
+                              //that answer as the first element in the "as" option.
                               q: "How would you like to answer this question?",
-                              as: ["Press 1 or click here for this answer.",
+                              as: ["Press 1 or click here for this answer.", //this would be the correct answer if hasCorrect:true
                                    "Press 2 or click here for this answer.",
                                    "Press 3 or click here for this answer."]}],
-    ["practice", "DashedSentence", {s: "This is the last practice sentence before the experiment begins."}],
 
-    //
-    // Two "real" (i.e. non-filler) self-paced reading items with corresponding acceptability judgment items.
-    // There are two conditions.
-    //
 
-    [["s1",1], "DashedSentence", {s: "The journalist interviewed an actress who he knew to be shy of publicity after meeting on a previous occasion."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-    [["s2",1], "DashedSentence", {s: "The journalist interviewed an actress who after meeting on a previous occasion he knew to be shy of publicity."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
+    //word by word text with fill in the blank question
+    ["story", "DashedSentence", {s: "Remember the story?"},
+     "Form", {html: 'Which player won the race: <input type="text" name="winningPlayer">'}],
 
-    // The first question will be chosen if the first sentence from the previous two items is chosen;
-    // the second question will be chosen if the second sentence from the previous pair of items is chosen.
-    [["q1",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?"}],
-    [["q2",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview her husband after meeting on a previous occasion?"}],
 
-    [["s1",2], "DashedSentence", {s: "The teacher helped struggling students who he encouraged to succeed without treating like idiots."},
-               "Question",       {q: "What did the teacher do?",
-                                  as: ["Encourage struggling students to succeed",
-                                       "Encourage his best students to succeed",
-                                       "Treat struffling students like idiots"]}],
-    [["s2",2], "DashedSentence", {s: "The teacher helped struggling students who without treating like idiots he encouraged to succeed."},
-               "Question",       {q: "What did the teacher do?", as: ["Encourage struggling students to succeed",
-                                                                      "Encourage his best students to succeed",
-                                                                      "Treat struggling students like idiots"]}],
+     //all text with scaling question
+     ["story", "AcceptabilityJudgment", {s:"From a scale of 1 to 9, how has your morning been?",
+                                           as:["1","2","3","4","5","6","7","8","9"]}],
 
-    [["q1",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage to succeed without treating their friends like idiots?"}}],
-    [["q2",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage their friends to succeed without treating like idiots?"}}],
 
-    //
-    // 10 self-paced-reading filler sentences.
-    //
+    /*
+    ===================
+    IMAGE
+    Controllers that work with Images and Questions
+    ===================
+    */
 
-    ["f", "DashedSentence", {s: "The foreign spy that encoded the top-secret messages was given a new mission that required going to Japan."},
-          "Question",       {q: "The spy's mission required him to:", as: ["Go to Japan", "Destroy top-secret messages", "Bug a hotel room"]}],
+     //image that needs to be clicked
+     ["image", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" />', transfer: "click"}],
 
-    ["f", "DashedSentence", {s: "The receptionist that the real estate company just hired immediately familiarized herself with all the phone numbers of their clients."},
-          "Question",       {q: "The receptionist familiarized herself with:",
-                             as: ["Some phone numbers",
-                                  "The health and safety regulations",
-                                  "Her boss"]}],
 
-    ["f", "DashedSentence", {s: "Only two specialized surgeons that work in the hospital could do this operation."},
-          "Question",       {q: "The operation can be performed by:",
-                             as: ["Two surgeons with specialist training",
-                                  "All the surgeons at the hospital",
-                                  "Three surgeons who are currently off sick"]}],
+     //image with keypress, was a question that has a correct answer
+     //let hasCorrect: false, if optinion based question
+     ["image", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" />', transfer: "keypress"},
+              "Question", {q: "Whats under 'Language'?", //the answer to this is "Knowlege"
+                            as: ["Knowledge", "Skill", "Math", "Research"]}],
 
-    ["f", "DashedSentence", {s: "The gangsters that the local police officers tracked for years were represented by an inexperienced lawyer."},
-          "Question",       {q: "Who did the inexperienced lawyer represent?",
-                             as: ["Some gangsters", "Some local police officers", "A murder suspect"]}],
+      //image that has a timer with a fill in question
+     ["image", "Message", {html:'<img src = "http://www.sjsu.edu/linguistics/pics/lld_wordle_660px.jpg" /><br><p>Please wait</p>', transfer: 10000},//look at image for 10secs
+        "Form", {html: 'Write something about image here: <input type="text" name="aboutImg">'}]
 
-    ["f", "DashedSentence", {s: "The woman that John had seen in the subway bought herself a pair of stunning shoes that cost a fortune."},
-          "Question",       {q: "Where did John see the woman?", as: ["In the subway", "On the bus", "In the shoe shop"]}],
-
-    ["f", "DashedSentence", {s: "If the award-winning chef had entered this competition, he surely would have won first prize."},
-          "Question",       {q: "Why didn't the chef win the competition?",
-                             as: ["Because he didn't enter it",
-                                  "Because his food wasn't good enough.",
-                                  "Because he was kicked out for cheating."]}],
-
-    ["f", "DashedSentence", {s: "If the organized secretary had filed the documents when she first received them, they would have been easy to find."},
-          "Question",       {q: "Why were the documents difficult to find?",
-                             as: ["Because the secretary hadn't filed them properly",
-                                  "Because a manager at the company had lost them",
-                                  "Because they had been stolen."]}],
-
-    ["f", "DashedSentence", {s: "If the homemade beer had been left to ferment more, it would have been drinkable."},
-          "Question",       {q: "Why wasn't the homemade beer drinkable?",
-                             as: ["It hadn't been left to ferment long enough",
-                                  "It had been left to ferment too long",
-                                  "The ingredients had been measured incorrectly."]}],
-
-    ["f", "DashedSentence", {s: "The cowboy that the bulls tried to trample injured himself getting off a horse."}],
-
-    ["f", "DashedSentence", {s: "The patient that was admitted to the hospital last month still suffers severe pain in his left leg."},
-          "Question",       {q: "Which of the following is true?",
-                             as: ["The patient still has severe pain in his left leg",
-                                  "The patient still has severe pain in his right leg",
-                                  "The patient no longer suffers from pain in his left leg"]}]
 ];
