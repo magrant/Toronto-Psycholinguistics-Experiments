56951da1cf9ee7bffa11aa20ce07250bc388f43d
Amanjit <amanjitsingh.kainth@mail.utoronto.ca>
2015-06-22 14:19:30 -0400

    Added files needed for sound

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,190 +1,91 @@
-var shuffleSequence = seq(/*"intro",*/ sepWith("sep", seq("story", rshuffle("s1", "s2"))), sepWith("sep", rshuffle("q1", "q2")));
-var practiceItemTypes = ["practice"];
-
+var shuffleSequence = sepWith("sep", not("sep"));
+var centerItems = true;
 
 var defaults = [
-    "Separator", {
-        transfer: 1000, //wait for 1000ms
-          //other options: "keypress", "click"
-        normalMessage: "Please wait for the next sentence.", //message to be displayed
-        errorMessage: "Wrong. Please wait for the next sentence." //message to be displayed in red
+  "Question", {
+    as: ["Yes", "No"]
+  },
+  //the deault options can
+  //be changed to any other options you would like to present
+  "DashedSentence", {
+    mode: "speeded acceptability"
+  },
+  //These settings are needed for audio Type 1
+  "AcceptabilityJudgment", {
+    // q: 'What did u hear?',
+    as: ["OK"],
+    presentAsScale: true,
+    instructions: "Use number keys or click boxes to answer.",
+    // leftComment: "(Bad)",
+    // rightComment: "(Good)",
+    //only two audio options available so far
+    audioMessage: {
+      html: "<u>Click here to play audio</u>"
     },
-
-    "Message", {
-        //"html" option is obligatory
-        hideProgressBar: false
-    },
-
-    "DashedSentence", {
-        //"s" option is obligatory
-        mode: "self-paced reading"
-          //other option: "speeded acceptability"
-    },
-
-    "FlashSentence", {
-      //"s" option is obligatory
-      transfer: "keypress"
-    },
-
-    "Question", {
-        //"as" option is obligatory
-        hasCorrect: true
-    },
-
-    "AcceptabilityJudgment", {
-        //"s" option is obligatory
-        //"q" option is obligatory
-        //"as" option is obligatory
-        as: ["1", "2", "3", "4", "5", "6", "7"],
-        //writing the "as" option here means that this is the default for
-        //all AcceptabilityJudgment items
-        presentAsScale: true, //presents the "as" option as a scale
-        instructions: "Use number keys or click boxes to answer.", //instruction text
-        leftComment: "(Bad)", //displayed on the left side of the scale
-        rightComment: "(Good)" //displayed on the right side of the scale
-    },
-
-    "DashedAcceptabilityJudgment", {
-        //combination of AcceptabilityJudgment and DashedSentence
-        //"s" option is obligatory
-        //"q" option is obligatory
-        //"as" option is obligatory
-        hasCorrect: false
-    },
-
-    "Form", {
-        //"html" option is obligatory
-        hideProgressBar: true,
-        continueOnReturn: true,
-        saveReactionTime: true
-    }
+    audioTrigger: "click"
+  } //do not change this
+  //click, we do have another option at this point of time
 ];
 
 var items = [
+  // Change the transfer to the number of milliseconds you want in between questions
+  // and change your normalMessage according to what you want the client
+  // to see while this break
+  ["sep", "Separator", {
+    transfer: 5,
+    normalMessage: "Please wait for the next item."
+  }],
+  //The following are text followed by questions
+  //You can create more instances of these by just adding more such arrays
+  //DashedSentence can be changed to FlashSentence
+  //another convenient way of doing this using AcceptabilityJudgment and
+  //its dash sentence version DashedAcceptibilityJudgment
+  ["q1", "DashedSentence", {
+      s: "Your text here"
+    },
+    "Question", {
+      q: "Your Question here"
+    }
+  ],
+  //Type 1 audio, this is for when you don't want participants
+  //to contol when to pause, this is just when the participant is ready,
+  //they click and the audio plays without any interruption.
+  //So in essential, you want to force participant to hear the entire audio
+  //the audio file must be in chunk_includes
+  ["q2", "AcceptabilityJudgment", {
+    s: {
+      audio: "test1.mp3"
+    }
+  }],
 
-    /*
-    ===================
-    SEPARATOR
-    The pause needed between each item of the experiment
-    ===================
-    */
+  // Just to check if participant can hear audio or No
+  ["q3", "Question", {
+    q: 'Did you hear anything?'
+  }],
 
-    //ends after timer (1000ms)
-    ["sep", "Separator", {transfer: 1000, normalMessage: "Please wait for the next sentence."}],
+  //Type 2 audio, autoplay, press a button to continue at any time
+  //participant can pause/play as many times as possible
 
-    //ends when key is press
-    ["sep", "Separator", {transfer: "keypress", normalMessage: "Please press any key to continue."}],
+  ["q4", "Message", {
+    html: {
+      include: 'sound_display.html'
+    },
+    // conosentRequired: "true",
+    // continueMessage: "Click here to contiue",
+    transfer: 'click'
+  }],
+
+  //Type 3 audio, pausable version, not able to pause/play audio
+  //just press any key to continue at any time
+  //for example, press any key as soon as sentence stops making sense
+
+  ["q5", "Message", {
+    html: {
+      include: 'sound_nodisplay.html'
+    },
+    transfer: 'keypress'
+  }]
 
 
-    /*
-    ===================
-    INTRODUCTION
-    Can include files for Questionnaires, consent forms etc...
-    ===================
-    */
 
-    //name of controller
-    ["intro",
-      //type
-      "Form",
-      //obligatory option that includes a HTML file that is a questionnaire
-      {html: { include: "example_intro.html" },
-      //fields that need to have the right format when taking input from user
-      validators: {
-        //age has to be a number
-        age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
-        }
-    } ],
-
-    //
-    // Three practice items for self-paced reading (one with a comprehension question).
-    //
-    ["practice", "DashedSentence", {s: "This is a practice sentence to get you used to reading sentences like this."}],
-    ["practice", "DashedSentence", {s: "This is another practice sentence with a practice question following it."},
-                 "Question", {hasCorrect: false, randomOrder: false,
-                              q: "How would you like to answer this question?",
-                              as: ["Press 1 or click here for this answer.",
-                                   "Press 2 or click here for this answer.",
-                                   "Press 3 or click here for this answer."]}],
-    ["practice", "DashedSentence", {s: "This is the last practice sentence before the experiment begins."}],
-
-    //
-    // Two "real" (i.e. non-filler) self-paced reading items with corresponding acceptability judgment items.
-    // There are two conditions.
-    //
-
-    [["s1",1], "DashedSentence", {s: "The journalist interviewed an actress who he knew to be shy of publicity after meeting on a previous occasion."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-    [["s2",1], "DashedSentence", {s: "The journalist interviewed an actress who after meeting on a previous occasion he knew to be shy of publicity."},
-               "Question",       {q: "The actress was:", as: ["shy", "publicity-seeking", "impatient"]}],
-
-    // The first question will be chosen if the first sentence from the previous two items is chosen;
-    // the second question will be chosen if the second sentence from the previous pair of items is chosen.
-    [["q1",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview after meeting her PA on a previous occasion?"}],
-    [["q2",[100,1]], "AcceptabilityJudgment", {s: "Which actress did the journalist interview her husband after meeting on a previous occasion?"}],
-
-    [["s1",2], "DashedSentence", {s: "The teacher helped struggling students who he encouraged to succeed without treating like idiots."},
-               "Question",       {q: "What did the teacher do?",
-                                  as: ["Encourage struggling students to succeed",
-                                       "Encourage his best students to succeed",
-                                       "Treat struffling students like idiots"]}],
-    [["s2",2], "DashedSentence", {s: "The teacher helped struggling students who without treating like idiots he encouraged to succeed."},
-               "Question",       {q: "What did the teacher do?", as: ["Encourage struggling students to succeed",
-                                                                      "Encourage his best students to succeed",
-                                                                      "Treat struggling students like idiots"]}],
-
-    [["q1",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage to succeed without treating their friends like idiots?"}}],
-    [["q2",[200,2]], "AcceptabilityJudgment", {s: {html: "<b>Which struggling students</b> did the teacher encourage their friends to succeed without treating like idiots?"}}],
-
-    //
-    // 10 self-paced-reading filler sentences.
-    //
-
-    ["f", "DashedSentence", {s: "The foreign spy that encoded the top-secret messages was given a new mission that required going to Japan."},
-          "Question",       {q: "The spy's mission required him to:", as: ["Go to Japan", "Destroy top-secret messages", "Bug a hotel room"]}],
-
-    ["f", "DashedSentence", {s: "The receptionist that the real estate company just hired immediately familiarized herself with all the phone numbers of their clients."},
-          "Question",       {q: "The receptionist familiarized herself with:",
-                             as: ["Some phone numbers",
-                                  "The health and safety regulations",
-                                  "Her boss"]}],
-
-    ["f", "DashedSentence", {s: "Only two specialized surgeons that work in the hospital could do this operation."},
-          "Question",       {q: "The operation can be performed by:",
-                             as: ["Two surgeons with specialist training",
-                                  "All the surgeons at the hospital",
-                                  "Three surgeons who are currently off sick"]}],
-
-    ["f", "DashedSentence", {s: "The gangsters that the local police officers tracked for years were represented by an inexperienced lawyer."},
-          "Question",       {q: "Who did the inexperienced lawyer represent?",
-                             as: ["Some gangsters", "Some local police officers", "A murder suspect"]}],
-
-    ["f", "DashedSentence", {s: "The woman that John had seen in the subway bought herself a pair of stunning shoes that cost a fortune."},
-          "Question",       {q: "Where did John see the woman?", as: ["In the subway", "On the bus", "In the shoe shop"]}],
-
-    ["f", "DashedSentence", {s: "If the award-winning chef had entered this competition, he surely would have won first prize."},
-          "Question",       {q: "Why didn't the chef win the competition?",
-                             as: ["Because he didn't enter it",
-                                  "Because his food wasn't good enough.",
-                                  "Because he was kicked out for cheating."]}],
-
-    ["f", "DashedSentence", {s: "If the organized secretary had filed the documents when she first received them, they would have been easy to find."},
-          "Question",       {q: "Why were the documents difficult to find?",
-                             as: ["Because the secretary hadn't filed them properly",
-                                  "Because a manager at the company had lost them",
-                                  "Because they had been stolen."]}],
-
-    ["f", "DashedSentence", {s: "If the homemade beer had been left to ferment more, it would have been drinkable."},
-          "Question",       {q: "Why wasn't the homemade beer drinkable?",
-                             as: ["It hadn't been left to ferment long enough",
-                                  "It had been left to ferment too long",
-                                  "The ingredients had been measured incorrectly."]}],
-
-    ["f", "DashedSentence", {s: "The cowboy that the bulls tried to trample injured himself getting off a horse."}],
-
-    ["f", "DashedSentence", {s: "The patient that was admitted to the hospital last month still suffers severe pain in his left leg."},
-          "Question",       {q: "Which of the following is true?",
-                             as: ["The patient still has severe pain in his left leg",
-                                  "The patient still has severe pain in his right leg",
-                                  "The patient no longer suffers from pain in his left leg"]}]
 ];
